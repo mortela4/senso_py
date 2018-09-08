96626e70da2185fe6292bb7e6b92f6574e3c1061
mortela4 <morten.larsen@7sense.no>
2018-09-08 19:19:05 +0200

    Added 'sensors_fwk' module - refactoring out actual sensor drivers into a) a module for actual sensor-drivers, and b) a module for mocked sensor-driver replacements

---
+++ source/sensor_drivers/mocked_sensor_driver.py
@@ -0,0 +1,39 @@
+
+
+
+# For demo purposes:
+# ==================
+class ComplexValue:
+    def __init__(self, triggered=False, channel=-1, ch_val=0.0):
+        self.triggered = triggered
+        self.channel = channel
+        self.ch_val = ch_val
+
+
+# static mock-up functions - could as well be @staticmethod-decorated methods in 'Sensors' class.
+# ===============================================================================================
+def configure_i2c_sensor(bus_no, addr):
+    print("MOCK: Configuring I2C-sensor on bus no.%d, address=%d ..." % (bus_no, addr))
+
+
+def configure_spi_sensor(bus_no, cs_num):
+    print("MOCK: Configuring SPI-sensor on bus no.%d, CS-num=%d..." % (bus_no, cs_num))
+
+
+def get_i2c_val():
+    print("MOCK: Getting I2C-sensor value ...")
+    # Returns a single value (which would typically be float-type):
+    return 1.12345
+
+
+def get_spi_val():
+    print("MOCK: Getting SPI-sensor value ...")
+    # Demonstrates returning a complex object:
+    return ComplexValue(True, 7, 8.765)
+
+
+def get_uart_val():
+    print("MOCK: Getting UART-sensor value ...")
+    # Demonstrate returning a list (of values), instead of a single value:
+    return [3, 4, 5]
+
